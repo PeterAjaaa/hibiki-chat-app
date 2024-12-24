7ffd999a1cc60159aca8dd54bc30e01e6e423a03
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-12-24 19:05:04 +0700

    Start working on API routes

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::get('user', [AuthController::class, 'getAuthenticatedUser']);
