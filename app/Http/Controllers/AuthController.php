6c1d122919870682ff668ceea104de740ae1f8b9
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-12-26 00:56:07 +0700

    Implement basic JWT authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    // Let the user register and return their the user and their JWT access token
+    // TODO: Add email verification on register, put the refresh token on user DB
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        } else {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+            $token = JWTAuth::fromUser($user);
+            return response()->json((compact('user', 'token')), 201);
+        }
+    }
+
+    // Let the user log in using their credentials
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([$validator->errors()], 400);
+        } else {
+            $credentials = $request->only('email', 'password');
+
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            } else {
+                return response()->json((compact('token')), 200);
+            }
+        }
+    }
+
+    // Log out the user by invalidating their JWT access token
+    public function logout()
+    {
+        JWTAuth::invalidate(JWTAuth::getToken());
+        return response()->json(['message' => 'Successfully logged out'], 200);
+    }
+
+    // Get authenticated user's details
+    public function getAuthenticatedUser()
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        return response()->json(compact('user'));
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,8 @@ use App\Http\Controllers\AuthController;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
-Route::post('logout', [AuthController::class, 'logout']);
-Route::get('user', [AuthController::class, 'getAuthenticatedUser']);
+
+Route::group(['middleware' => ['jwt.auth']], function () {
+    Route::get('user', [AuthController::class, 'getAuthenticatedUser']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
