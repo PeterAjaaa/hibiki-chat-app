2550c8a9740fba3283eea347d864db618eadb394
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-12-27 18:02:21 +0700

    Add more API endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ConversationController;
+use App\Http\Controllers\MessageController;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
@@ -9,4 +11,11 @@ Route::post('register', [AuthController::class, 'register']);
 Route::group(['middleware' => ['jwt.auth']], function () {
     Route::get('user', [AuthController::class, 'getAuthenticatedUser']);
     Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::get('conversations', [ConversationController::class, 'index']);
+    Route::post('conversations', [ConversationController::class, 'create']);
+    Route::get('conversations/{id}', [ConversationController::class, 'show']);
+    Route::post('conversations/{id}/participants', [ConversationController::class, 'addParticipant']);
+    Route::delete('conversations/{id}/participants', [ConversationController::class, 'removeParticipant']);
+    Route::delete('conversations/{id}', [ConversationController::class, 'destroy']);
 });
