1f96e8ea14db29b83038b3dbdbc1fb06145c0f77
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-12-27 18:00:29 +0700

    Update the conversation migration file, to reflect the new relationship and table schema

--- database/migrations/2024_12_27_002859_create_conversations_table.php
+++ database/migrations/2024_12_27_002859_create_conversations_table.php
@@ -13,11 +13,9 @@ return new class extends Migration
     {
         Schema::create('conversations', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id_one')->constrained('users')->onDelete('cascade');
-            $table->foreignId('user_id_two')->constrained('users')->onDelete('cascade');
+            $table->string('title')->nullable(); // Optional, for group chats
+            $table->boolean('is_group')->default(false);
             $table->timestamps();
-
-            $table->unique(['user_id_one', 'user_id_two']);
         });
     }
 
