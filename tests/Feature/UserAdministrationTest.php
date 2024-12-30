2b9a1ed78718e9d4e85ed78255beacf5999ecef3
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-12-30 23:41:10 +0700

    Add more comprehensive test to test out the register endpoint

--- tests/Feature/UserAdministrationTest.php
+++ tests/Feature/UserAdministrationTest.php
@@ -39,11 +39,208 @@ final class UserAdministrationTest extends TestCase
         ]);
     }
 
+    public function testUserRegistrationMissingRequiredField()
+    {
+        $userData = [
+            'email' => 'testuser@example.com',
+            'password' => 'passwordTestUser',
+            'password_confirmation' => 'passwordTestUser'
+        ];
+
+        $response = $this->post('/api/register', $userData);
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonStructure(
+                [
+                    'success',
+                    'message',
+                    'error' => [
+                        'error_details',
+                    ]
+                ]
+            );
+    }
+
+    public function testUserRegistrationFailingValidation()
+    {
+        $userData = [
+            'name' => 'Test User',
+            'email' => 'testuser@example.com',
+            'password' => 'passwordTestUser',
+            'password_confirmation' => 'passwordTestUserIsNotTheSame'
+        ];
+
+        $response = $this->post('/api/register', $userData);
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonStructure(
+                [
+                    'success',
+                    'message',
+                    'error' => [
+                        'error_details',
+                    ]
+                ]
+            );
+    }
+
+    public function testUserRegistrationNonUniqueEmail()
+    {
+        $oldUser = User::factory()->create([
+            'email' => 'testuser@example.com',
+        ]);
+
+        $userData = [
+            'name' => 'Test User',
+            'email' => 'testuser@example.com',
+            'password' => 'passwordTestUser',
+            'password_confirmation' => 'passwordTestUserIsNotTheSame'
+        ];
+
+        $response = $this->post('/api/register', $userData);
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonStructure(
+                [
+                    'success',
+                    'message',
+                    'error' => [
+                        'error_details',
+                    ]
+                ]
+            );
+    }
+
+    public function testUserRegistrationIncorrectDataType()
+    {
+        $userData = [
+            'name' => 'Test User',
+            'email' => 'testuser@example.com',
+            'password' => 12345678,
+            'password_confirmation' => 'passwordTestUserIsNotTheSame'
+        ];
+
+        $response = $this->post('/api/register', $userData);
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonStructure(
+                [
+                    'success',
+                    'message',
+                    'error' => [
+                        'error_details',
+                    ]
+                ]
+            );
+    }
+
+    public function testUserRegistrationInvalidPasswordLength()
+    {
+        $userData = [
+            'name' => 'Test User',
+            'email' => 'testuser@example.com',
+            'password' => '123',
+            'password_confirmation' => 'passwordTestUserIsNotTheSame'
+        ];
+
+        $response = $this->post('/api/register', $userData);
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonStructure(
+                [
+                    'success',
+                    'message',
+                    'error' => [
+                        'error_details',
+                    ]
+                ]
+            );
+    }
+
+    public function testUserRegistrationMalformedEmail()
+    {
+        $userData = [
+            'name' => 'Test User',
+            'email' => 'testuser.example.com',
+            'password' => 'passwordTest',
+            'password_confirmation' => 'passwordTest'
+        ];
+
+        $response = $this->post('/api/register', $userData);
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonStructure(
+                [
+                    'success',
+                    'message',
+                    'error' => [
+                        'error_details',
+                    ]
+                ]
+            );
+    }
+
+    public function testUserRegistrationOverlyLongName()
+    {
+        $userData = [
+            'name' => Str::random(100),
+            'email' => 'testuser@example.com',
+            'password' => 'passwordTest',
+            'password_confirmation' => 'passwordTest'
+        ];
+
+        $response = $this->post('/api/register', $userData);
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonStructure(
+                [
+                    'success',
+                    'message',
+                    'error' => [
+                        'error_details',
+                    ]
+                ]
+            );
+    }
+
+    public function testUserRegistrationOverlyLongPassword()
+    {
+        $password = Str::random(100);
+
+        $userData = [
+            'name' => 'Test User',
+            'email' => 'testuser@example.com',
+            'password' => $password,
+            'password_confirmation' => $password
+        ];
+
+        $response = $this->post('/api/register', $userData);
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonStructure(
+                [
+                    'success',
+                    'message',
+                    'error' => [
+                        'error_details',
+                    ]
+                ]
+            );
+    }
+
     public function testUserLogin()
     {
         $user = User::factory()->create([
             'email' => 'test@example.com',
-            'password' => bcrypt('passwordTest'),
+            'password' => Hash::make('passwordTest')
         ]);
 
         $response = $this->postJson('/api/login', [
