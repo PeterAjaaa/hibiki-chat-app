974d2bca7e0c067c9c4f26529a26e5aea1f919e1
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-12-31 01:06:37 +0700

    Create a new test suite for testing login endpoint and to separate concern

---
+++ tests/Feature/UserLoginTest.php
@@ -0,0 +1,154 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+final class UserLoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testUserLogin()
+    {
+        User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('passwordTest')
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 'passwordTest',
+        ]);
+
+        $response
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'success',
+                'message',
+                'data' => [
+                    'token'
+                ]
+            ]);
+
+        $responseData = $response->json();
+        $this->assertNotEmpty($responseData['data']['token'], 'Token should not be empty');
+    }
+
+    public function testUserLoginMissingRequiredField()
+    {
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)->assertJsonStructure([
+            'success',
+            'message',
+            'error' => [
+                'error_details'
+            ]
+        ]);
+    }
+
+
+    public function testUserLoginIncorrectDataType()
+    {
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 12345678,
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)->assertJsonStructure([
+            'success',
+            'message',
+            'error' => [
+                'error_details'
+            ]
+        ]);
+    }
+
+    public function testUserLoginOutOfRange()
+    {
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => '',
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)->assertJsonStructure([
+            'success',
+            'message',
+            'error' => [
+                'error_details'
+            ]
+        ]);
+    }
+
+    public function testUserLoginWrongCredential()
+    {
+        User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('passwordTestCorrect')
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 'passwordTestWrong',
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED)->assertJsonStructure([
+            'success',
+            'message',
+            'error' => [
+                'error_details'
+            ]
+        ]);
+    }
+
+    public function testUserLoginMalformedEmail()
+    {
+        $response = $this->postJson('/api/login', [
+            'email' => 'test.example.com',
+            'password' => 'passwordTest',
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)->assertJsonStructure([
+            'success',
+            'message',
+            'error' => [
+                'error_details'
+            ]
+        ]);
+    }
+
+    public function testUserOverlyLongEmail()
+    {
+        $response = $this->postJson('/api/login', [
+            'email' => Str::random(100),
+            'password' => 'passwordTest',
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)->assertJsonStructure([
+            'success',
+            'message',
+            'error' => [
+                'error_details'
+            ]
+        ]);
+    }
+
+    public function testUserOverlyLongPassword()
+    {
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => Str::random(100)
+        ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)->assertJsonStructure([
+            'success',
+            'message',
+            'error' => [
+                'error_details'
+            ]
+        ]);
+    }
+}
