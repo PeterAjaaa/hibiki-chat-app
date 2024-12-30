03faa3318192d019dd1679fa13fe0dc6622cf79d
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-12-30 19:33:39 +0700

    Write test for register and login API endpoint

---
+++ tests/Feature/UserAdministrationTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+final class UserAdministrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testUserRegistration()
+    {
+        $userData = [
+            'name' => 'Test User',
+            'email' => 'testuser@example.com',
+            'password' => 'passwordTestUser',
+            'password_confirmation' => 'passwordTestUser'
+        ];
+
+        $response = $this->post('/api/register', $userData);
+
+        $response
+            ->assertStatus(Response::HTTP_CREATED)
+            ->assertJsonStructure(
+                [
+                    'success',
+                    'message',
+                    'data' => [
+                        'user',
+                        'token',
+                    ]
+                ]
+            );
+
+        $this->assertDatabaseHas('users', [
+            'name' => $userData['name'],
+            'email' => $userData['email']
+        ]);
+    }
+
+    public function testUserLogin()
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => bcrypt('passwordTest'),
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 'passwordTest',
+        ]);
+
+        $response->assertStatus(Response::HTTP_OK)->assertJsonStructure([
+            'success',
+            'message',
+            'data' => [
+                'token',
+            ]
+        ]);
+    }
+}
