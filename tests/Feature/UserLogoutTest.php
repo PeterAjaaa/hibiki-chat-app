03421ea8ac333321ebc7b6662ece688c2653d3ce
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-12-31 02:45:42 +0700

    Add a new test suite to test the logout API endpoint

---
+++ tests/Feature/UserLogoutTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+final class UserLogoutTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testUserLogout()
+    {
+        $user = User::factory()->create();
+        $token = auth('api')->login($user);
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->post('api/logout');
+
+        $response
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'success',
+                'message',
+            ]);
+    }
+
+    public function testUserLogoutUnauthorized()
+    {
+        // This header is needed so that the we can assert the JSON structure
+        $response = $this->withHeaders(['Accept' => 'application/json'])->get('api/user');
+
+        // Middleware-based exception has their own way of handling exception
+        $response
+            ->assertStatus(401)
+            ->assertJsonStructure(
+                [
+                    'message',
+                ]
+            );
+    }
+}
