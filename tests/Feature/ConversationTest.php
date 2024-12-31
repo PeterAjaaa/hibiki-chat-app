50ca2ceecaa053b6effba85fdc5cc31f4fb6d4bb
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2025-01-01 06:24:55 +0700

    Add new test suite to test the conversation endpoint

--- tests/Feature/ConversationTest.php
+++ tests/Feature/ConversationTest.php
@@ -28,7 +28,7 @@ final class ConversationTest extends TestCase
 
         $response
             ->assertStatus(Response::HTTP_OK)
-            ->assertJsonStructure([
+            ->assertExactJsonStructure([
                 'success',
                 'message',
                 'data' => [
@@ -44,5 +44,147 @@ final class ConversationTest extends TestCase
         $this->assertDatabaseHas('conversations', [
             'id' => $response['data']['conversation']['id']
         ]);
+    }
+
+    public function testCreateNewConversationMissingRequiredField()
+    {
+
+        $sender = User::factory()->create();
+        $token = auth('api')->login($sender);
+
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->get('api/user');
+
+        // This header is needed so that the we can assert the JSON structure
+        $response = $this->withHeaders(['Accept' => 'application/json'])->get('api/user');
+
+        $response = $this->postJson(
+            '/api/conversations',
+            [
+                'user_ids' => []
+            ]
+        );
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonValidationErrors('user_ids')
+            ->assertExactJsonStructure([
+                'errors' => [
+                    '0' => 'user_ids',
+                ],
+                'message',
+            ]);
+    }
+
+    public function testCreateNewConversationMissingPartialRequiredField()
+    {
+
+        $sender = User::factory()->create();
+        $token = auth('api')->login($sender);
+
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->get('api/user');
+
+        // This header is needed so that the we can assert the JSON structure
+        $response = $this->withHeaders(['Accept' => 'application/json'])->get('api/user');
+
+        $response = $this->postJson(
+            '/api/conversations',
+            [
+                'user_ids' => [$sender->id]
+            ]
+        );
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonValidationErrors('user_ids')
+            ->assertExactJsonStructure([
+                'errors' => [
+                    '0' => 'user_ids',
+                ],
+                'message',
+            ]);
+    }
+
+
+    public function testCreateNewConversationIncorrectDataType()
+    {
+        $sender = User::factory()->create();
+        $receiver = User::factory()->create();
+        $token = auth('api')->login($sender);
+
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->get('api/user');
+
+        // This header is needed so that the we can assert the JSON structure
+        $response = $this->withHeaders(['Accept' => 'application/json'])->get('api/user');
+
+        $response = $this->postJson(
+            '/api/conversations',
+            [
+                'user_ids' => ['1', '2']
+            ]
+        );
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonValidationErrors(['user_ids.0'])
+            ->assertJsonValidationErrors(['user_ids.1'])
+            ->assertExactJsonStructure([
+                'errors' => [
+                    '0' => 'user_ids.0',
+                    '1' => 'user_ids.1'
+                ],
+                'message',
+            ]);
+    }
+
+    public function testCreateNewConversationValueOutOfRange()
+    {
+        $sender = User::factory()->create();
+        $receiver = User::factory()->create();
+        $token = auth('api')->login($sender);
+
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)->get('api/user');
+
+        // This header is needed so that the we can assert the JSON structure
+        $response = $this->withHeaders(['Accept' => 'application/json'])->get('api/user');
+
+        $response = $this->postJson(
+            '/api/conversations',
+            [
+                'user_ids' => [$sender->id, $receiver->id + 1]
+            ]
+        );
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonValidationErrors(['user_ids.1'])
+            ->assertExactJsonStructure([
+                'errors' => [
+                    '0' => 'user_ids.1',
+                ],
+                'message',
+            ]);
+    }
+
+    public function testUserProfileUnauthorized()
+    {
+        // This header is needed so that the we can assert the JSON structure
+        $response = $this->withHeaders(['Accept' => 'application/json'])->post('api/conversations');
+
+        // Middleware-based exception has their own way of handling exception
+        $response
+            ->assertStatus(Response::HTTP_UNAUTHORIZED)
+            ->assertExactJsonStructure(
+                [
+                    'exception',
+                    'file',
+                    'line',
+                    'message',
+                    'trace',
+                ]
+            );
     }
 }
